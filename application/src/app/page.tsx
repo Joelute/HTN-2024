c0c7597cd9af9196598ea927940fdf8c9fd6ce01
Anna Valentino <pusheen1308@gmail.com>
2024-09-14 18:22:06 -0400

    touch up the homepage and fans database page

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React from 'react';
+import {Flex, Button, Text, Icon, ChakraProvider} from '@chakra-ui/react';
+import { FaSmile } from 'react-icons/fa'; // Example icon from react-icons
+import Link from "next/link";
 
-const HomePage = () =>
-{
+
+const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <ChakraProvider>
+            <Flex minH="100vh" flexDirection="column" px={4}>
+                {/* Header */}
+                <Flex
+                    justifyContent="space-between"
+                    alignItems="center"
+                    padding="2rem"
+                    position="absolute"
+                    width="100%"
+                    top={0}
+                    left={0}
+                    right={0}
+                >
+                    <img src="../passes.png" alt="passes" style={{ height: '40px' }} />
+                    <Text color="black" fontSize="lg" fontWeight={"bold"}>
+                        welcome, creator
+                    </Text>
+                </Flex>
+
+                {/* Main Content */}
+                <Flex
+                    minH="calc(100vh - 60px)" // Adjust height to fit the header
+                    justifyContent="center"
+                    alignItems="center"
+                    flexDirection="column"
+                    textAlign="center"
+                >
+                    {/*<Icon as={FaSmile} boxSize={12} color="blue.500" mb={4} />*/}
+                    <Link href='/fanabase' passHref>
+                        <Button
+                            colorScheme="purple"
+                            size="lg"
+                            variant="solid"
+                        >
+                            start chatting with your amazing fans
+                        </Button>
+                    </Link>
+                </Flex>
+            </Flex>
+        </ChakraProvider>
+
     );
 }
 
-export default HomePage
+export default HomePage;
