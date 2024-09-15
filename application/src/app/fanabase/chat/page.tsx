9e6bed931654120aeee54523c5c7a295ce169efc
Logan Fossenier <logan.fossenier@gmail.com>
2024-09-15 02:40:51 -0400

    fix: make npm build by listening to eslint

--- application/src/app/fanabase/chat/page.tsx
+++ application/src/app/fanabase/chat/page.tsx
@@ -1,131 +1,139 @@
-'use client';
+"use client";
 
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { FaArrowLeft, FaUserCircle } from 'react-icons/fa';
+
 import {
-    Box,
-    Button,
-    Drawer,
-    DrawerBody,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    Flex,
-    Heading,
-    IconButton,
-    Text,
-    useDisclosure,
-    Avatar,
-    Input,
-    Stack, ChakraProvider
+    Avatar, Box, Button, ChakraProvider, Drawer, DrawerBody, DrawerCloseButton, DrawerContent,
+    DrawerHeader, DrawerOverlay, Flex, Heading, IconButton, Input, Stack, Text, useDisclosure
 } from '@chakra-ui/react';
-import {FaArrowLeft, FaUserCircle} from 'react-icons/fa';
-import Link from "next/link";
 
 const ChatRoom = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure(); // Chakra UI hook for drawer
+  const { isOpen, onOpen, onClose } = useDisclosure(); // Chakra UI hook for drawer
 
-    const [userDetails, setUserDetails] = useState({
-        name: '',
-        nickname: '',
-        age: '',
-        email: ''
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    nickname: "",
+    age: "",
+    email: "",
+  });
+
+  const [message, setMessage] = useState(""); // State to hold the current message
+  const [messages, setMessages] = useState<string[]>([]); // State to hold the list of messages
+
+  // TODO remove me
+  useEffect(() => {
+    // Set user details only when the component mounts
+    setUserDetails({
+      name: "John Doe",
+      nickname: "John",
+      age: "25",
+      email: "he@gmail.com",
     });
+  }, []); // Empty dependency array ensures this runs only once
 
-    const [message, setMessage] = useState(''); // State to hold the current message
-    const [messages, setMessages] = useState<string[]>([]); // State to hold the list of messages
+  const handleSendMessage = () => {
+    if (message.trim()) {
+      setMessages([...messages, message]); // Add new message to the list
+      setMessage(""); // Clear the input field
+    }
+  };
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSendMessage(); // Send message on Enter key press
+    }
+  };
 
-    const handleSendMessage = () => {
-        if (message.trim()) {
-            setMessages([...messages, message]); // Add new message to the list
-            setMessage(''); // Clear the input field
-        }
-    };
+  return (
+    <ChakraProvider>
+      <Box p={4}>
+        {/* Chat Room Header */}
+        {/* Chat Room Header */}
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          mb={4}
+          bg="gray.100"
+          p={4}
+          borderRadius="md"
+        >
+          <Flex alignItems="center">
+            <Link href={"/fanabase"} passHref>
+              <IconButton
+                icon={<FaArrowLeft />}
+                aria-label="Back"
+                size="lg"
+                mr={4}
+              />
+            </Link>
+            <Heading size="md">{userDetails.nickname || "User"}</Heading>
+          </Flex>
+          <IconButton
+            icon={<FaUserCircle />}
+            aria-label="Profile"
+            onClick={onOpen}
+            size="lg"
+          />
+        </Flex>
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
-            handleSendMessage(); // Send message on Enter key press
-        }
-    };
+        {/* Chat window */}
+        <Box
+          border="1px"
+          borderColor="gray.300"
+          borderRadius="md"
+          p={4}
+          height="400px"
+          bg="gray.50"
+          overflowY="auto"
+          mb={4}
+        >
+          <Stack spacing={3}>
+            {messages.map((msg, index) => (
+              <Text key={index}>{msg}</Text> // Display each message
+            ))}
+          </Stack>
+        </Box>
 
-    return (
-        <ChakraProvider>
-            <Box p={4}>
-                {/* Chat Room Header */}
-                {/* Chat Room Header */}
-                <Flex justifyContent="space-between" alignItems="center" mb={4} bg="gray.100" p={4} borderRadius="md">
-                    <Flex alignItems="center">
-                        <Link href={'/fanabase'} passHref>
-                            <IconButton
-                                icon={<FaArrowLeft />}
-                                aria-label="Back"
-                                size="lg"
-                                mr={4}
-                            />
-                        </Link>
-                        <Heading size="md">{userDetails.nickname || 'User'}</Heading>
-                    </Flex>
-                    <IconButton
-                        icon={<FaUserCircle />}
-                        aria-label="Profile"
-                        onClick={onOpen}
-                        size="lg"
-                    />
-                </Flex>
+        {/* Message input and send button */}
+        <Flex>
+          <Input
+            placeholder="Type a message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeyDown}
+            mr={2}
+          />
+          <Button colorScheme="blue" onClick={handleSendMessage}>
+            Send
+          </Button>
+        </Flex>
 
-                {/* Chat window */}
-                <Box
-                    border="1px"
-                    borderColor="gray.300"
-                    borderRadius="md"
-                    p={4}
-                    height="400px"
-                    bg="gray.50"
-                    overflowY="auto"
-                    mb={4}
-                >
-                    <Stack spacing={3}>
-                        {messages.map((msg, index) => (
-                            <Text key={index}>{msg}</Text> // Display each message
-                        ))}
-                    </Stack>
-                </Box>
+        {/* User Info Side Panel */}
+        <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>{userDetails.nickname}&#39;s Profile</DrawerHeader>
 
-                {/* Message input and send button */}
-                <Flex>
-                    <Input
-                        placeholder="Type a message..."
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        onKeyDown={handleKeyDown}
-                        mr={2}
-                    />
-                    <Button colorScheme="blue" onClick={handleSendMessage}>
-                        Send
-                    </Button>
-                </Flex>
-
-                {/* User Info Side Panel */}
-                <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-                    <DrawerOverlay />
-                    <DrawerContent>
-                        <DrawerCloseButton />
-                        <DrawerHeader>{userDetails.nickname}'s Profile</DrawerHeader>
-
-                        <DrawerBody>
-                            <Avatar name={userDetails.name} mb={4} />
-                            <Text><strong>Name:</strong> {userDetails.name}</Text>
-                            <Text><strong>Age:</strong> {userDetails.age}</Text>
-                            <Text><strong>Email:</strong> {userDetails.email}</Text>
-                        </DrawerBody>
-                    </DrawerContent>
-                </Drawer>
-            </Box>
-        </ChakraProvider>
-
-    );
+            <DrawerBody>
+              <Avatar name={userDetails.name} mb={4} />
+              <Text>
+                <strong>Name:</strong> {userDetails.name}
+              </Text>
+              <Text>
+                <strong>Age:</strong> {userDetails.age}
+              </Text>
+              <Text>
+                <strong>Email:</strong> {userDetails.email}
+              </Text>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Box>
+    </ChakraProvider>
+  );
 };
 
 export default ChatRoom;
--- application/src/app/fanabase/page.tsx
+++ application/src/app/fanabase/page.tsx
@@ -1,128 +1,109 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import {
-    Container,
-    Flex,
-    Input,
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    Button,
-    Heading,
-    ChakraProvider,
-    useDisclosure,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Textarea,
-} from '@chakra-ui/react';
-import { FaSearch } from 'react-icons/fa';
-import Link from "next/link";
-import { api } from '@/../convex/_generated/api';
 import { useQuery } from 'convex/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
 
-type User = {
-    _id: number
-    _creationTime: number;
-    name: string;
-    email: string;
-    interests: string[];
-    username: string;
-    summarizedData: string;
-} 
+import { api } from '@/../convex/_generated/api';
+import {
+    Button, ChakraProvider, Container, Flex, Heading, Input, Modal, ModalBody, ModalContent,
+    ModalFooter, ModalHeader, ModalOverlay, Table, Tbody, Td, Textarea, Th, Thead, Tr, useDisclosure
+} from '@chakra-ui/react';
+
+import passes from '../../../public/passes.png';
 
 const Fanabase = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const data = useQuery(api.queries.getAllUsers.getAllUsers)
-    const { isOpen, onOpen, onClose } = useDisclosure(); // Chakra UI hook for modal
+  const [searchTerm, setSearchTerm] = useState("");
+  const data = useQuery(api.queries.getAllUsers.getAllUsers);
+  const { isOpen, onOpen, onClose } = useDisclosure(); // Chakra UI hook for modal
 
-    const filteredData = data?.filter(user =>
-        user.username.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    
-    return (
-        <ChakraProvider>
-            <Container maxW="container.xl" p={4}>
-                <Flex direction="column" minH="100vh">
-                    <Flex
-                        justifyContent="space-between"
-                        alignItems="center"
-                        mb={6}
-                        padding="1rem"
-                    >
-                        <Flex alignItems="center">
-                            <img src="../passes.png" alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
-                            <Heading size="lg">Fanabase</Heading>
-                        </Flex>
-                        <Input
-                            placeholder="Search by username"
-                            width="300px"
-                            value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
-                        />
-                    </Flex>
+  const filteredData = data?.filter((user) =>
+    user.username.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-                    <Table variant="striped">
-                        <Thead>
-                            <Tr>
-                                <Th>Name</Th>
-                                <Th>Username</Th>
-                                <Th>Email</Th>
-                                <Th>Start Chatting</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                        {filteredData && filteredData.length > 0 ? filteredData.map(user =>( 
-                            <Tr>
-                                <Td>{user.name}</Td>
-                                <Td>{user.username}</Td>
-                                <Td>{user.email}</Td>
-                                <Td>
-                                        <Link href={`/fanabase/chat?name=${user.name}&nickname=${user.username}&email=${user.email}`} passHref>
-                                        <Button colorScheme="pink" size="sm">
-                                            Chat
-                                        </Button>
-                                        </Link>
-                                </Td>
-                            </Tr>)) : null}
-                        </Tbody>
-                    </Table>
-                    <Flex justifyContent="flex-end" mt={4}>
-                        <Button colorScheme="purple" size="md" onClick={onOpen}>
-                            Massive Send ✨
-                        </Button>
-                    </Flex>
+  return (
+    <ChakraProvider>
+      <Container maxW="container.xl" p={4}>
+        <Flex direction="column" minH="100vh">
+          <Flex
+            justifyContent="space-between"
+            alignItems="center"
+            mb={6}
+            padding="1rem"
+          >
+            <Flex alignItems="center">
+              <Image src={passes} alt="passes" width={40} height={40} />
+              <Heading size="lg">Fanabase</Heading>
+            </Flex>
+            <Input
+              placeholder="Search by username"
+              width="300px"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </Flex>
 
-                    {/* Massive Send Modal */}
-                    <Modal isOpen={isOpen} onClose={onClose}>
-                        <ModalOverlay />
-                        <ModalContent>
-                            <ModalHeader>Massive Send ✨</ModalHeader>
-                            <ModalBody>
-                                <Textarea
-                                    placeholder="Write your message here..."
-                                    size="md"
-                                    mb={4}
-                                />
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button colorScheme="blue" onClick={onClose}>
-                                    Send to All
-                                </Button>
-                            </ModalFooter>
-                        </ModalContent>
-                    </Modal>
-                </Flex>
-            </Container>
-        </ChakraProvider>
-    );
+          <Table variant="striped">
+            <Thead>
+              <Tr>
+                <Th>Name</Th>
+                <Th>Username</Th>
+                <Th>Email</Th>
+                <Th>Start Chatting</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {filteredData && filteredData.length > 0
+                ? filteredData.map((user) => (
+                    <Tr key={user._id}>
+                      <Td>{user.name}</Td>
+                      <Td>{user.username}</Td>
+                      <Td>{user.email}</Td>
+                      <Td>
+                        <Link
+                          href={`/fanabase/chat?name=${user.name}&nickname=${user.username}&email=${user.email}`}
+                          passHref
+                        >
+                          <Button colorScheme="pink" size="sm">
+                            Chat
+                          </Button>
+                        </Link>
+                      </Td>
+                    </Tr>
+                  ))
+                : null}
+            </Tbody>
+          </Table>
+          <Flex justifyContent="flex-end" mt={4}>
+            <Button colorScheme="purple" size="md" onClick={onOpen}>
+              Massive Send ✨
+            </Button>
+          </Flex>
+
+          {/* Massive Send Modal */}
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Massive Send ✨</ModalHeader>
+              <ModalBody>
+                <Textarea
+                  placeholder="Write your message here..."
+                  size="md"
+                  mb={4}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button colorScheme="blue" onClick={onClose}>
+                  Send to All
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </Flex>
+      </Container>
+    </ChakraProvider>
+  );
 };
 
 export default Fanabase;
