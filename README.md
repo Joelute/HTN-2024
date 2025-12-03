b392b0cda1d0a9db82695313861170b882988b2e
Logan Fossenier <logan.fossenier@gmail.com>
2025-12-02 20:31:23 -0600

    chore: deploy locally to record gif

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # HTN-2024
+
+Our project was an idea inspired by Lucy Guo; in-CRM personalized mass messaging for content creators + chat functionality.
+
+![Demo](images/demo.gif)
+
+This video is a loop of our landing page loading, and I stopped the video immediately after entering a personalized chat.
+
+Basically, content creators could have a tailored AI persona (in this repo implemented using Groq AI) that could discuss with the users and they could "interact" with the content creator on some tier-based plan.
+
+This allows content creators to monetize their persona outside of content creation, as well as send mass messages written by them specifically too. The plan is higher engagement, higher audience retention.
--- application/convex/_generated/api.d.ts
+++ application/convex/_generated/api.d.ts
@@ -1,5 +1,3 @@
-/* prettier-ignore-start */
-
 /* eslint-disable */
 /**
  * Generated `api` utility.
@@ -10,11 +8,6 @@
  * @module
  */
 
-import type {
-  ApiFromModules,
-  FilterApi,
-  FunctionReference,
-} from "convex/server";
 import type * as mutations_addCommunication from "../mutations/addCommunication.js";
 import type * as mutations_addUser from "../mutations/addUser.js";
 import type * as mutations_deleteCommunication from "../mutations/deleteCommunication.js";
@@ -23,14 +16,12 @@ import type * as queries_getAllUsers from "../queries/getAllUsers.js";
 import type * as queries_getUserById from "../queries/getUserById.js";
 import type * as queries_getUserCommunications from "../queries/getUserCommunications.js";
 
-/**
- * A utility for referencing Convex functions in your app's API.
- *
- * Usage:
- * ```js
- * const myFunctionReference = api.myModule.myFunction;
- * ```
- */
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
 declare const fullApi: ApiFromModules<{
   "mutations/addCommunication": typeof mutations_addCommunication;
   "mutations/addUser": typeof mutations_addUser;
@@ -40,13 +31,31 @@ declare const fullApi: ApiFromModules<{
   "queries/getUserById": typeof queries_getUserById;
   "queries/getUserCommunications": typeof queries_getUserCommunications;
 }>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
 
-/* prettier-ignore-end */
+export declare const components: {};
